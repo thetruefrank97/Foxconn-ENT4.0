7513a5679cfb84d1289bc34587ea060ebee5d806
sasanyny <43596981+sasanyny@users.noreply.github.com>
2020-05-04 04:55:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'assets')))
   .set('views', path.join(__dirname, 'foxTest1'))
-  .set('view engine', 'ejs')
+  .set('view engine', 'js')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
